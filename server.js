8d0d3529f4a9d09e688201f47fca5d0c9385801e
github username <github email address>
2021-03-25 17:02:20 -0400

    hashed users passwrod and add validations

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init MiddleWare: lets us accept data 
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({msg: "Welcome to the Promise Keeper API..."}));
 
