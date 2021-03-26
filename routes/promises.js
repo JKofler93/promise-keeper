bea916b21463639a672b74c9308402738b0ddd36
github username <github email address>
2021-03-26 17:14:53 -0400

    full crud actions working on postman

--- routes/promises.js
+++ routes/promises.js
@@ -59,15 +59,59 @@ async (req, res) => {
 // @route   PUT    api/promises/:id
 // @desc    Edit promises
 // @access  Private
-router.put('/:id', (req, res) => {
-    res.send('Edit promises')
+router.put('/:id', auth, async (req, res) => {
+    // grab what we want
+    const { content, completed } = req.body;
+
+    // Make a promise Object to see if fields are submitted
+    const promiseFields = {};
+    if(content) promiseFields.content = content;
+    if(completed) promiseFields.completed = completed;
+    
+    try {
+        // req has a params, the parameters we want to find is ID
+        let promise = await Promise.findById(req.params.id);
+        // Check if the promise exists 
+        if(!promise) return res.status(404).json({ msg: 'Promise not found'});
+        // Check promise belongs to user
+        if(promise.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized'})
+        }
+
+        // .findByIdAndUpdate takes 3 parameters the promiseId
+        promise = await Promise.findByIdAndUpdate(req.params.id, 
+            // Set the promiseFields
+            { $set: promiseFields },
+            // If promise doesn't exist, create it
+            { new: true });
+        
+            res.json(promise);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   DELETE    api/promises/:id
 // @desc    Delete promises
 // @access  Private
-router.delete('/:id', (req, res) => {
-    res.send('Delete promises')
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let promise = await Promise.findById(req.params.id);
+ 
+        if(!promise) return res.status(404).json({ msg: 'Promise not found'});
+ 
+        if(promise.user.toString() !== req.user.id) {
+         return res.status(401).json({ msg: 'Not authorized'})
+        }
+ 
+        await Promise.findByIdAndRemove(req.params.id);
+ 
+         res.json({ msg: 'Promise Removed'});
+    } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
